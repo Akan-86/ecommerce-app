c0a7f9a9c05579f622bc681916717ba398011669
Akan-86 <akan.degirmenci@gmail.com>
2025-11-11 21:16:22 +0300

    chore(firebase): initialize Firebase app with Firestore, Auth, and Storage using env.local configuration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -16,6 +16,6 @@ const firebaseConfig = {
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
-export const auth = getAuth(app); // ✅ Bu satır eksikti
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
