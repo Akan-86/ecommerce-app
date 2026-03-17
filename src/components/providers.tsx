85d752a73a1aea5e2f86c7b91b1e499eceb1897e
Akan-86 <akan.degirmenci@gmail.com>
2026-03-17 21:51:39 +0300

    refactor providers to include all client-side logic including navbar and footer

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,7 +2,30 @@
 
 import { ReactNode } from "react";
 import { LanguageProvider } from "@/context/language-context";
+import { AuthProvider } from "@/context/auth-context";
+import { ToastProvider } from "@/context/toast-context";
+import { CartProvider } from "@/context/cart-context";
+import Navbar from "@/components/navbar";
+import { Footer } from "@/components/home/footer";
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return <LanguageProvider>{children}</LanguageProvider>;
+  return (
+    <LanguageProvider>
+      <AuthProvider>
+        <ToastProvider>
+          <CartProvider>
+            <div className="flex min-h-screen flex-col">
+              {/* Navbar is here */}
+              <Navbar />
+
+              <main className="flex-1">{children}</main>
+
+              {/* Footer is here */}
+              <Footer />
+            </div>
+          </CartProvider>
+        </ToastProvider>
+      </AuthProvider>
+    </LanguageProvider>
+  );
 }
